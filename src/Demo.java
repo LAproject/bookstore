57158113da0ee0d1c5fcaa3b14642f91ff3256b3
ARTE <ARTE@10.7.56.62>
2017-03-14 14:00:13 +0000

    main picture added

--- src/Demo.java
+++ src/Demo.java
@@ -1,23 +1,37 @@
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.HeadlessException;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.imageio.ImageIO;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 	public class Demo extends JFrame implements ActionListener {
 		
@@ -31,19 +45,35 @@ import javax.swing.JTextField;
 	    Connection con = null;
 	    static Container mainContainer; 
 	 
-	    public Demo()
+	    public Demo() throws IOException 
 	    {
-	    	
-			panel = mainPanel();
-			mainContainer = getContentPane();
-						
+	    	mainContainer = getContentPane();
+	    	panel = mainPanel();
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnsupportedLookAndFeelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+					
 			mainContainer.add(panel);		
 			setVisible(true);
 			setSize(500,500);
-			setLocation(300,300);
+			setLocation(250,150);
 	    }
+	     
 	    
-	    @Override
+
+		@Override
 		public void actionPerformed(ActionEvent e) {
 	    	if (e.getActionCommand().equals("Next")){
 	    		CardLayout card = (CardLayout)cardDisplay.getLayout();
@@ -61,7 +91,12 @@ import javax.swing.JTextField;
 	    	if (e.getActionCommand().equals("Quit")){
 	    		
 	    		mainContainer.removeAll();
-				mainContainer.add(mainPanel());
+				try {
+					mainContainer.add(mainPanel());
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				frame.validate();
     		}
 		}
@@ -210,15 +245,20 @@ import javax.swing.JTextField;
 			}
 	    }
 	    
-    	public JPanel mainPanel(){
+    	public JPanel mainPanel() throws IOException{
     		
     		isAdmin = true;
-    		mainPanel = new JPanel();
-			
-    		mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.X_AXIS));	
-			
+    		JPanel header = new JPanel();
+    		header.setLayout(new BoxLayout(header,BoxLayout.Y_AXIS));
+    		JPanel buttonsPanel = new JPanel();
+    		buttonsPanel.setLayout(new BoxLayout(buttonsPanel,BoxLayout.X_AXIS));
+			mainPanel= new JPanel();
+    		//mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));	
+    		BufferedImage img = ImageIO.read(new File("/Users/arturwac/Desktop/advanced/bookstore/src/bgimage.png"));
+	    
 			JLabel welcome = new JLabel("WELCOME");
-			
+			Font font = new Font("Verdana", Font.BOLD, 30);
+			welcome.setFont(font);
 			login = new JButton("LOGIN");
 			exit = new JButton("EXIT");
 			register = new JButton("REGISTER");
@@ -226,10 +266,16 @@ import javax.swing.JTextField;
 			register.addActionListener(this);
 			exit.addActionListener(this);
 			
-			mainPanel.add(welcome);
-			mainPanel.add(login);
-			mainPanel.add(register);
-			mainPanel.add(exit);
+			
+			buttonsPanel.add(login);
+			buttonsPanel.add(register);
+			buttonsPanel.add(exit);
+			header.add(welcome);
+			
+			mainPanel.add(header, BorderLayout.NORTH );
+			mainPanel.add(new JLabel(new ImageIcon(img)), BorderLayout.CENTER);
+			mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+			
     		
 			return mainPanel;
     		
@@ -382,9 +428,10 @@ import javax.swing.JTextField;
 	   	return adminPanel; 
 	   }
 
-	    public static void main (String[] args) {
+	    public static void main (String[] args) throws IOException {
 	     
 	        frame = new Demo();
+	        
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    }
 	
